02e20483f59654b6f5d54dd4b895711b9a0a4754
Tam Dao <>
2022-11-06 01:17:31 +0700

    Redireact all page to books page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './providers'
 import { Auth, Books } from './pages'
 import { Layout, RequireAuth } from './components'
@@ -19,6 +19,7 @@ function App() {
           />
         </Route>
         <Route element={<Auth />} path="/auth" />
+        <Route element={<Navigate to="/books" />} path="*" />
       </Routes>
     </AuthProvider>
   )
