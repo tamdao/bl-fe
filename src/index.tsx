f713fd1cba2e3b4ed1fc6288316624bc6bdea2fe
Tam Dao <>
2022-11-05 19:19:50 +0700

    Add react router, react query and mantine package

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { Global, MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+import { ModalsProvider } from '@mantine/modals'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Global
+          styles={() => ({
+            '*, *::before, *::after': {
+              boxSizing: 'border-box',
+            },
+
+            body: {
+              fontSize: 14,
+            },
+          })}
+        />
+        <BrowserRouter>
+          <ModalsProvider>
+            <App />
+          </ModalsProvider>
+        </BrowserRouter>
+      </MantineProvider>
+    </QueryClientProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
